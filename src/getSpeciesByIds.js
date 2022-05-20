57c9a26232ab7a1f423dcd9cfe21b469a3129e28
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-20 16:33:30 -0300

    requisito-1-concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-unreachable */
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.reduce((elemento1, elemento2) => (elemento1.concat(species.find(
+    (callback) => callback.id === elemento2,
+  ))), []);
 }
 
 module.exports = getSpeciesByIds;
