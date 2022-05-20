751c0a24d57a7f4a06086d8e4fc6d60ad9aa6ed9
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-20 18:46:59 -0300

    requisito-3-concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const names = employees
+    .find((elemento) => elemento.firstName === employeeName || elemento.lastName === employeeName);
+  return (names === undefined) ? {} : names;
 }
 
 module.exports = getEmployeeByName;
