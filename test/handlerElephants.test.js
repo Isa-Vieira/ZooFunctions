e955505dd5dced10d8d12b95e8edf516c5463b1b
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-23 16:09:57 -0300

    requisito-7-15-concluidos

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -13,3 +13,21 @@ describe('Testes da função HandlerElephants', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
   });
 });
+it('Retorna uma string NW', () => {
+  expect(handlerElephants('location')).toBe('NW');
+});
+it('Retorna um número igual ou maior a 5', () => {
+  expect(handlerElephants('popularity')).toBe(5);
+});
+it('Retorna um array', () => {
+  expect(handlerElephants('availability')).not.toEqual('Monday');
+});
+it('Retorna undefined', () => {
+  expect(handlerElephants(undefined)).toBe(undefined);
+});
+it('Retorna uma string', () => {
+  expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+});
+it('Retorna null', () => {
+  expect(handlerElephants('')).toBe(null);
+});
