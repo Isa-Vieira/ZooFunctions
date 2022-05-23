ed835de7fff037f56e697f1ee1ccc845a064d896
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-23 14:42:13 -0300

    requisito-4-concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((elemento) => elemento.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const filtrar = employees.filter((elemento) => elemento.managers.includes(managerId));
+    const filtraAndReduce = filtrar.reduce(
+      (elemento1, elemento2) => elemento1.concat(
+        (`${elemento2.firstName} ${elemento2.lastName}`),
+      ), [],
+    );
+    return filtraAndReduce;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
