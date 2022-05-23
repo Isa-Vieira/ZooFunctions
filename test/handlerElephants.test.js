68e346109930786a1e271c05ca04350051fbc864
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-23 15:23:06 -0300

    requisito-6-concluido

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,15 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Para o argumento count deve retornar o número 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Retorna um array de nomes que possui o nome Jefferson', () => {
+    const arrayAnimais = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+
+    expect(handlerElephants('names')).toEqual(arrayAnimais);
+  });
+  it('Retorna um número próximo de 10.5', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+});
