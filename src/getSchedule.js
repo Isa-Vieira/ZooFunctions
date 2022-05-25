a065a3a9f262776a6b139ec2fcea7d1e9b65e236
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-25 19:17:04 -0300

    lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,16 +1,7 @@
-const { hours, species } = require('../data/zoo_data');
+/* const { hours, species } = require('../data/zoo_data'); */
 const data = require('../data/zoo_data');
 
-/*  const officeHour = hours;
-const exhibition =  especies; */
-
-
 function getSchedule(scheduleTarget) {
-/* if(species.name === name)
-return [] hours.data
-if(species === ())
-return dias e animais disponiveis */
-}
-/* Array.concat */
 
+}
 module.exports = getSchedule;
