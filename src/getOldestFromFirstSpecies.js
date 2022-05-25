b725ee33676d156f4eb1e96e40107f4d00b930c4
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-25 18:53:57 -0300

    requisito-10-concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees, species } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const idAnimal = employees.find((person) => person.id === id).responsibleFor[0];
+  return Object.values(species.find((animal) => animal.id === idAnimal).residents
+    .reduce((param1, param2) => {
+      if (param1.age > param2.age) {
+        return param1;
+      }
+      return param2;
+    }));
+}
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,16 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+/*  const officeHour = hours;
+const exhibition =  especies; */
+
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+/* if(species.name === name)
+return [] hours.data
+if(species === ())
+return dias e animais disponiveis */
 }
+/* Array.concat */
 
 module.exports = getSchedule;
