3d0b1806b19ce304f43d271350415fbf01c4f371
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-26 19:30:35 -0300

    requisito-9-concluido

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
-/* const { hours, species } = require('../data/zoo_data'); */
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const { hours, species } = data;
 
+const arrayAnimais = species.map((item) => item.name);
+
+const officeHour = (objeto) => {
+  if (objeto.open === 0) return 'CLOSED';
+  return `Open from ${objeto.open}am until ${objeto.close}pm`;
+};
+
+const exhibition = (elemento) => {
+  if (elemento === 'Monday') return 'The zoo will be closed!';
+  return species.filter((item) => item.availability.includes(elemento))
+    .map((item) => item.name);
+};
+
+const scheduleHouers = {
+  Tuesday: { officeHour: officeHour(hours.Tuesday), exhibition: exhibition('Tuesday') },
+  Wednesday: { officeHour: officeHour(hours.Wednesday), exhibition: exhibition('Wednesday') },
+  Thursday: { officeHour: officeHour(hours.Thursday), exhibition: exhibition('Thursday') },
+  Friday: { officeHour: officeHour(hours.Friday), exhibition: exhibition('Friday') },
+  Saturday: { officeHour: officeHour(hours.Saturday), exhibition: exhibition('Saturday') },
+  Sunday: { officeHour: officeHour(hours.Sunday), exhibition: exhibition('Sunday') },
+  Monday: { officeHour: officeHour(hours.Monday), exhibition: exhibition('Monday') },
+};
+
+function getSchedule(scheduleTarget) {
+  if (arrayAnimais.includes(scheduleTarget)) {
+    return species.find((item) => item.name === scheduleTarget).availability;
+  }
+  if (Object.keys(hours).includes(scheduleTarget)) {
+    return { [scheduleTarget]: scheduleHouers[scheduleTarget] };
+  }
+  return scheduleHouers;
 }
+
 module.exports = getSchedule;
